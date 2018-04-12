78555e1032a37b24402e29e42e8f2c89e443fd98
Leticia Wanderley <leticiafwanderley@gmail.com>
2018-04-12 11:49:47 -0300

    Add get button and footer interaction

--- react/components/Footer.js
+++ react/components/Footer.js
@@ -4,7 +4,10 @@ class Footer extends Component {
   render() {
     const year = new Date().getFullYear()
     return (
-      <footer className="static-s fixed-ns z-3 w-100 bottom-0 pa7 bg-white flex items-center justify-center-s justify-start-ns light-gray">
+      <footer
+        id="extension-store-footer"
+        className="static-s fixed-ns z-3 w-100 bottom-0 pa7 flex items-center justify-center-s justify-start-ns bg-light-silver gray"
+      >
         <span className="mh2">&copy; {year}</span>
         <span>VTEX Extension Store</span>
       </footer>
--- react/components/ProductDescription.js
+++ react/components/ProductDescription.js
@@ -4,22 +4,52 @@ import PropTypes from 'prop-types'
 import GetButton from './GetButton'
 
 class ProductDescription extends Component {
+  state = {
+    fixed: true,
+  }
+
   static propTypes = {
     description: PropTypes.string.isRequired,
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.watchScroll)
+    this.watchScroll()
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.watchScroll)
+  }
+
+  watchScroll = () => {
+    const scroll = window.pageYOffset
+    const scrollHeight = document.body.scrollHeight
+    const footerSize = document.getElementById('extension-store-footer')
+      .offsetHeight
+    const fixed =
+      document.documentElement.offsetHeight > window.innerHeight
+        ? scroll < scrollHeight - footerSize - window.innerHeight
+        : false
+    if (fixed !== this.state.fixed) {
+      this.setState({
+        fixed,
+      })
+    }
   }
 
   render() {
     const { description } = this.props
     return (
       <div className="mh6-s mh0-ns near-black f5">
-        <div className="pb10-ns">
-          {description}
-          {description}
-          {description}
-          {description}
-        </div>
-        <div className="bottom-0 ma4 z-2 db-s dn-ns b--white get-button-shadow">
-          <GetButton />
+        <div className="pb3-s pb10-ns">{description}</div>
+        <div className="h3">
+          <div
+            className={`bottom-0 left-0 w-100 z-2 db-s dn-ns b--white bb bw2 get-button-shadow bg-white ${
+              this.state.fixed ? 'fixed pb4 ph6' : ''
+            }`}
+          >
+            <GetButton />
+          </div>
         </div>
       </div>
     )
