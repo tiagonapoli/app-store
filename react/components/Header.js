38da0d357ad86afdc78efd764adf3cf928975ce1
Leticia Wanderley <leticiafwanderley@gmail.com>
2018-05-08 21:56:46 -0300

    Fix search bar tilt

--- react/components/Header.js
+++ react/components/Header.js
@@ -23,7 +23,7 @@ class Header extends Component {
     scroll: 0,
     shouldShowSearch: true,
     headerSize: 64,
-    jumbontronSize: 600,
+    jumbontronSize: 890,
     store: '',
     shouldShowLoginModal: false,
   }
