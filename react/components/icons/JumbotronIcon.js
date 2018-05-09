8a53e5a0ee870fda9f5e65025db937a2de6b072e
Leticia Wanderley <leticiafwanderley@gmail.com>
2018-05-09 14:46:05 -0300

    Fix jumbotron icon

--- react/components/icons/JumbotronIcon.js
+++ react/components/icons/JumbotronIcon.js
@@ -12,21 +12,87 @@ class JumbotronIcon extends PureComponent {
       >
         <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
           <g
-            transform="translate(12.000000, 30.000000)"
+            transform="translate(0.000000, 28.000000)"
             stroke="#F71963"
             strokeWidth="2"
           >
-            <rect x="86" y="223" width="106" height="92" rx="4" />
-            <rect x="106" y="80" width="46" height="61" rx="4" />
-            <rect x="180" y="236" width="46" height="61" rx="4" />
-            <rect x="18" y="159" width="134" height="46" rx="4" />
-            <rect x="74" y="61" width="46" height="46" rx="4" />
-            <rect x="45" y="95" width="47" height="46" rx="4" />
-            <rect x="244" y="223" width="47" height="46" rx="4" />
-            <rect x="1" y="81" width="26" height="26" rx="4" />
-            <rect x="170" y="71" width="46" height="134" rx="4" />
-            <rect x="186" y="1" width="106" height="92" rx="4" />
-            <rect x="234" y="111" width="136" height="94" rx="4" />
+            <rect
+              x="86.9172478"
+              y="225.395635"
+              width="107.165444"
+              height="93.0143682"
+              rx="4"
+            />
+            <rect
+              x="107.133071"
+              y="80.8525009"
+              width="46.5179752"
+              height="61.6798425"
+              rx="4"
+            />
+            <rect
+              x="181.931616"
+              y="238.53592"
+              width="46.5179752"
+              height="61.6798425"
+              rx="4"
+            />
+            <rect
+              x="18.1834496"
+              y="160.705002"
+              width="135.467597"
+              height="46.5179752"
+              rx="4"
+            />
+            <rect
+              x="74.787754"
+              y="61.647469"
+              width="46.5179752"
+              height="46.5179752"
+              rx="4"
+            />
+            <rect
+              x="45.4748106"
+              y="96.0143682"
+              width="47.5287664"
+              height="46.5179752"
+              rx="4"
+            />
+            <rect
+              x="246.62225"
+              y="225.395635"
+              width="47.5287664"
+              height="46.5179752"
+              rx="4"
+            />
+            <rect
+              x="1"
+              y="81.8632921"
+              width="26.3021522"
+              height="26.3021522"
+              rx="4"
+            />
+            <rect
+              x="171.823704"
+              y="71.7553806"
+              width="46.5179752"
+              height="135.467597"
+              rx="4"
+            />
+            <rect
+              x="187.996363"
+              y="1"
+              width="107.165444"
+              height="93.0143682"
+              rx="4"
+            />
+            <rect
+              x="236.514338"
+              y="112.187027"
+              width="137.489179"
+              height="95.0359505"
+              rx="4"
+            />
           </g>
         </g>
       </svg>
