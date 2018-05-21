002eeb16d1851e04c7605f14f35be57ee81a0ec8
Leticia Wanderley <leticiafwanderley@gmail.com>
2018-05-21 10:09:49 -0300

    Update ul content

--- react/AppShelf.js
+++ react/AppShelf.js
@@ -42,7 +42,7 @@ class AppShelf extends Component {
       slidesToShow: 3,
       slidesToScroll: SLIDES_TO_SCROLL_LARGE_VIEWPORT,
       dots: DOTS_LARGE_VIEWPORT,
-      touchThreshold: 30,
+      touchThreshold: -50,
       arrows: false,
       responsive: [
         {
