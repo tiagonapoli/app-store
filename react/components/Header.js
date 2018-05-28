90767d3c6c6edea9e70f84c46eaec7ef896e764c
ntmedina <nataliamedina.rj@gmail.com>
2018-05-28 18:31:10 -0300

    Add google analytics script

--- react/components/Header.js
+++ react/components/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { injectIntl, intlShape } from 'react-intl'
-import { Link } from 'render'
+import { Link, Helmet } from 'render'
 
 import VTEXIcon from './icons/VTEXIcon'
 import BackIcon from './icons/BackIcon'
@@ -72,6 +72,16 @@ class Header extends Component {
     const titleClasses = notHome ? 'dn db-ns' : 'db'
     return (
       <div className="fixed-ns w-100 z-2">
+      <Helmet>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-43760863-24"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-43760863-24');
+        </script>
+      </Helmet>
         <div
           id="extension-store-header"
           className="flex justify-between items-center w-100 top-0 ph4 ph7-ns pv4 pv5-ns bg-serious-black tc tl-ns white"
