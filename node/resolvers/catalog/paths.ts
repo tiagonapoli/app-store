7737e87e695d3b81e6f9cf74951aa09ea6d7c3ce
Leticia Wanderley <leticiafwanderley@gmail.com>
2018-06-07 18:50:04 -0300

    Remove node folder

--- node/resolvers/catalog/paths.ts
+++
@@ -1,6 +0,0 @@
-export const paths = {
-  crossSelling: (account, id, type) =>
-    `http://${account}.vtexcommercestable.com.br/api/catalog_system/pub/products/crossselling/${type}/${id}`,
-  product: (account, { slug }) =>
-    `http://${account}.vtexcommercestable.com.br/api/catalog_system/pub/products/search/${slug}/p`,
-}
