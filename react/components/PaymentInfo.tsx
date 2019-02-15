0d6124425ea18e106210a9b7a0179cb45622f7f2
artur.pimentel <artur.oliveira@vtex.com.br>
2019-02-15 14:43:36 -0200

    Add state to payment info

--- react/components/PaymentInfo.tsx
+++ react/components/PaymentInfo.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, SFC } from 'react'
 import { InjectedIntlProps, injectIntl } from 'react-intl'
 import { Dropdown, Input } from 'vtex.styleguide'
 
@@ -6,7 +6,27 @@ export interface PaymentInfoProps {
   free: boolean
 }
 
-class PaymentInfo extends Component<PaymentInfoProps & InjectedIntlProps> {
+const initialState = {
+  cardHolder: '',
+  cardNumber: '',
+  city: '',
+  complement: '',
+  expirationMonth: '',
+  expirationYear: '',
+  number: '',
+  securityCode: '',
+  state: '',
+  street: '',
+  zip: '',
+}
+
+type PaymentInfoState = typeof initialState
+
+class PaymentInfo extends Component<PaymentInfoProps & InjectedIntlProps, PaymentInfoState> {
+  state = initialState
+
+  setFormState = <S extends keyof PaymentInfoState>(stateProperty: S) =>
+    (e: React.ChangeEvent<HTMLInputElement>) => this.setState<S>({ [stateProperty]: e.target.value })
 
   public render () {
     const { formatMessage } = this.props.intl
@@ -25,30 +45,49 @@ class PaymentInfo extends Component<PaymentInfoProps & InjectedIntlProps> {
                 <p className="mt0 mb3">
                   {formatMessage({ id: 'extensions.checkout.payment-info.card-number' })}
                 </p>
-                <div className="mb5"><Input /></div>
+                <div className="mb5">
+                  <Input
+                    value={this.state.cardNumber}
+                    onChange={this.setFormState('cardNumber')}
+                  />
+                </div>
                 <p className="mt0 mb3">
                   {formatMessage({ id: 'extensions.checkout.payment-info.card-holder' })}
                 </p>
-                <div className="mb5"><Input /></div>
+                <div className="mb5">
+                <Input
+                  value={this.state.cardHolder}
+                  onChange={this.setFormState('cardHolder')}
+                /></div>
                 <p className="mt0 mb3">
                   {formatMessage({ id: 'extensions.checkout.payment-info.expiration-date' })}
                 </p>
                 <div className="flex justify-center mb5">
                     <div className="mr5">
                       <Input
+                        value={this.state.expirationMonth}
                         placeholder={formatMessage({ id: 'extensions.checkout.payment-info.expiration-date.placeholder.month' })}
+                        onChange={this.setFormState('expirationMonth')}
                       />
                     </div>
                     <div>
                       <Input
+                        value={this.state.expirationYear}
                         placeholder={formatMessage({ id: 'extensions.checkout.payment-info.expiration-date.placeholder.year' })}
+                        onChange={this.setFormState('expirationYear')}
                       />
                     </div>
                 </div>
                 <p className="mt0 mb3">
                   {formatMessage({ id: 'extensions.checkout.payment-info.security-code' })}
                 </p>
-                <div className="w-50 pr3"><Input /></div>
+                <div className="w-50 pr3">
+                  <Input
+                    value={this.state.securityCode}
+                    placeholder="123"
+                    onChange={this.setFormState('securityCode')}
+                  />
+                </div>
               </div>
               <p className="mt0 mb7">
                 {formatMessage({ id: 'extensions.checkout.payment-info.billing-address.title' })}
@@ -58,13 +97,19 @@ class PaymentInfo extends Component<PaymentInfoProps & InjectedIntlProps> {
                   <p className="mt0 mb3">
                     {formatMessage({ id: 'extensions.checkout.payment-info.billing-address.zip' })}
                   </p>
-                  <Input />
+                  <Input
+                    value={this.state.zip}
+                    onChange={this.setFormState('zip')}
+                  />
                 </div>
                 <div className="w-66">
                   <p className="mt0 mb3">
                     {formatMessage({ id: 'extensions.checkout.payment-info.billing-address.address' })}
                   </p>
-                  <Input />
+                  <Input
+                    value={this.state.street}
+                    onChange={this.setFormState('street')}
+                  />
                 </div>
               </div>
               <div className="flex mb5">
@@ -72,13 +117,19 @@ class PaymentInfo extends Component<PaymentInfoProps & InjectedIntlProps> {
                   <p className="mt0 mb3">
                     {formatMessage({ id: 'extensions.checkout.payment-info.billing-address.number' })}
                   </p>
-                  <Input />
+                  <Input
+                    value={this.state.number}
+                    onChange={this.setFormState('number')}
+                  />
                 </div>
                 <div className="w-66">
                   <p className="mt0 mb3">
                     {formatMessage({ id: 'extensions.checkout.payment-info.billing-address.address-2' })}
                   </p>
-                  <Input />
+                  <Input
+                    value={this.state.complement}
+                    onChange={this.setFormState('complement')}
+                  />
                 </div>
               </div>
               <div className="flex mb5">
@@ -86,17 +137,22 @@ class PaymentInfo extends Component<PaymentInfoProps & InjectedIntlProps> {
                   <p className="mt0 mb3">
                     {formatMessage({ id: 'extensions.checkout.payment-info.billing-address.city' })}
                   </p>
-                  <Input />
+                  <Input
+                    value={this.state.city}
+                    onChange={this.setFormState('city')}
+                  />
                 </div>
                 <div className="w-33">
                   <p className="mt0 mb3">
                     {formatMessage({ id: 'extensions.checkout.payment-info.billing-address.state' })}
                   </p>
                   <Dropdown
+                    value={this.state.state}
                     options={[
                       { value: 'RJ', label: 'RJ' },
                       { value: 'SP', label: 'SP' },
                     ]}
+                    onChange={this.setFormState('state')}
                   />
                 </div>
               </div>
